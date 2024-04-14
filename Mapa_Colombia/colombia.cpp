79255d325a06fc8ffe4d6f8c76a01fd73b5a5e05
Juanegoma <juanesteb.gomez@urosario.edu.co>
2024-04-14 11:24:57 -0500

    Create colombia.cpp

---
+++ Mapa_Colombia/colombia.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <map>
+using namespace std;
+
+
+//typedef, se usa para simplificar el codigo.
+typedef par<string, string > par1;
+
+int main{
+
+
+//Los mapas simepre tomaran pares de valores.
+map<string, string > colombia;
+
+colombia.insert( par1("Amazonas","Leticia"));
+colombia.insert( par1("Antioquia","Medellin"));
+colombia.insert( par1("Arauca","Arauca"));
+colombia.insert( par1("Atlantico","Barranquilla"));
+colombia.insert( par1("Bogota","Bogota"));
+colombia.insert( par1("Bolivar","Cartagena"));
+colombia.insert( par1("Boyaca","Tunja"));
+colombia.insert( par1("Caldas","Manizales"));
+colombia.insert( par1("Caqueta","Florencia"));
+colombia.insert( par1("Casanare","Yopal"));
+colombia.insert( par1("Cauca","Popayan"));
+colombia.insert( par1("Cesar","Valledupar"));
+colombia.insert( par1("Choco","Quibdo"));
+colombia.insert( par1("Cordoba","Monteria"));
+colombia.insert( par1("Cundinamarca","Bogota"));
+colombia.insert( par1("Guainia","Inirida"));
+colombia.insert( par1("Guaviare","San Jose del Guaviare"));
+colombia.insert( par1("Huila","Neiva"));
+colombia.insert( par1("Guajira","Riohacha"));
+colombia.insert( par1("Magdalena","Santa Marta"));
+colombia.insert( par1("Meta","Villavicencio"));
+colombia.insert( par1("Nariño","Pasto"));
+colombia.insert( par1("Norte de Santander", "Cucuta"));
+colombia.insert( par1("Putumayo","Mocoa"));
+colombia.insert( par1("Quindio","Armenia"));
+colombia.insert( par1("Risaralda","Pereira"));
+colombia.insert( par1("San Andres","San Andres"));
+colombia.insert( par1("Santander","Bucaramanga"));
+colombia.insert( par1("Sucre","Sincelejo"));
+colombia.insert( par1("Tolima","Ibague"));
+colombia.insert( par1("Valle del Cauca","Cali"));
+colombia.insert( par1("Vaupes","Mitu"));
+colombia.insert( par1("Vichada","Puerto Carreño"));
+}
